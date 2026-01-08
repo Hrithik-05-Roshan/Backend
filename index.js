0b7894a0655a85459d3756fb57b38b9ac4cad18b
Hrithik Burnwal <hrithikrocks124@gmail.com>
2026-01-08 15:22:46 +0530

    Add Express server setup with middleware and routes

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+
+
+app.use(morgan('dev'))
+
+app.use(express.json())                         // built-in middleware
+app.use(express.urlencoded({extended: true}))   // built-in middleware
+
+
+app.set("view engine", 'ejs')
+
+
+app.use((req, res, next) => {
+    console.log("This is middleWare!");
+    return next()
+})
+
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+
+
+app.get('/about', (req,res)=>{
+    res.send('About Page!')
+})
+
+
+
+app.get('/profile', (req,res)=>{
+    res.send('Profile Page!')
+})
+
+
+// app.get('/get-form-data', (req,res)=>{    // in get method, data will show in the frontend in url
+//     console.log(req.query)
+//     res.send('data received!')
+// })
+
+app.post('/get-form-data', (req,res)=>{     // but if we use post it will not show the data!
+    console.log(req.body)
+    res.send('data received!')
+})
+
+app.listen(5000)
