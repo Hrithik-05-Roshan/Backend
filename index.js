66dd670469b9a4ebf748351fbc98753766e5b5da
Hrithik Burnwal <hrithikrocks124@gmail.com>
2026-01-08 15:42:22 +0530

    Add static file serving and initial styles/scripts
    - Set up static file serving for the 'public' directory in Express
    - Add CSS styles for the body in index.css
    - Include JavaScript alert in script.js
    - Link CSS and JavaScript files in the main EJS view

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(morgan('dev'))
 
 app.use(express.json())                         // built-in middleware
 app.use(express.urlencoded({extended: true}))   // built-in middleware
-
+app.use(express.static(("public")))
 
 app.set("view engine", 'ejs')
 
